335acbbf9f104565de08398aa9b746226ce3f0ea
HusanJava <husanboysobirjonov02@gmail.com>
2025-04-14 23:09:26 +0500

    2

--- core/settings.py
+++ core/settings.py
@@ -72,12 +72,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'voting_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Abdusami1602@',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
